ad706aedaf98419dbdee931d4c218ddb1b7a67d7
unknown <stagiaire@W2003IES.LOCAL>
2026-01-22 14:02:29 +0100

    {Switch Json} Controleur Livre et Mouvement

--- src/Controller/LivreController.php
+++ src/Controller/LivreController.php
@@ -1,238 +1,102 @@
 <?php
 
-/* @author : Dufour Marc (marc.dufour@stjosup.com)
- * @version : 1
- * @dateCreate : 12/01/2026
- * @lastUpdate : 19/01/2026
- */
-
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Attribute\Route;
-
-use App\Repository\LivreRepository;
 use App\Entity\Livre;
-use App\Entity\Category;
 use App\Form\LivreType;
+use App\Repository\LivreRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\Security\Http\Attribute\IsGranted;
-
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 #[Route('/livre')]
 class LivreController extends AbstractController
 {
-    private $session;
-
-    public function __construct(RequestStack $requestStack)
+    /**
+     * API : Vérifie l'existence d'un livre (BDD locale puis Google Books).
+     * Interagit avec : Le script JavaScript de la page d'accueil (index.html.twig).
+     */
+    #[Route('/api/verif-isbn/{isbn}', name: 'app_livre_verif', methods: ['GET'])]
+    public function verifierIsbn(string $isbn, LivreRepository $livreRepository, HttpClientInterface $clientHttp): JsonResponse
     {
-        $this->session = $requestStack->getSession();
-    }
-
-
-    #[Route('/', name: 'app_livre_index', methods: ['GET'])]
-    public function index(Request $request, LivreRepository $livreRepository): Response
-    {
-        return $this->render('livre/index.html.twig', [
-            'livres' => $livreRepository->findAll(),
-            'last_isbn' => $request->query->get('isbn'),
-            'last_auteur' => $request->query->get('auteur'),
-        ]);
-    }
-
-    #[Route('/{id<\d+>}', name: 'app_livre_show', methods: ['GET'])]
-    public function show(Livre $livre, Request $request): Response
-    {
-        // Si c'est une requête AJAX, on ne renvoie que le fragment Twig
-        if ($request->isXmlHttpRequest()) {
-            return $this->render('livre/_livre_details.html.twig', [
-                'livre' => $livre
-            ]);
-        }
-
-        // Sinon, on renvoie la page complète habituelle 
-        return $this->render('livre/show.html.twig', [
-            'livre' => $livre,
-        ]);
-    }
-
-    #[Route('/new', name: 'app_livre_new')]
-    public function new(Request $request, EntityManagerInterface $gestionnaireEntite): Response
-    {
-        $livre = new Livre();
-        
-        // 1. Récupération des paramètres depuis l'URL (envoyés par checkGoogleBooks)
-        $isbnPreRempli        = $request->query->get('isbn_pre_rempli');
-        $titrePreRempli       = $request->query->get('titre_pre_rempli');
-        $auteurPreRempli      = $request->query->get('auteur_pre_rempli');
-        $descriptionPreRemplie = $request->query->get('description_pre_remplie');
-        $imagePreRemplie      = $request->query->get('image_pre_remplie');
-
-        if ($isbnPreRempli) {
-            $livre->setISBN($isbnPreRempli);
-            $livre->setNbStock(1);
-        }
-        
-        if ($titrePreRempli) {
-            $livre->setNom($titrePreRempli);
-        }
-        
-        if ($auteurPreRempli) {
-            $livre->setAuteur($auteurPreRempli);
-        }
-
-        if ($descriptionPreRemplie) {
-            $livre->setDescription($descriptionPreRemplie);
-        }
-
-        if ($imagePreRemplie) {
-            $livre->setLienImg($imagePreRemplie); 
-        }
-        
-
-        $formulaire = $this->createForm(LivreType::class, $livre);
-        $formulaire->handleRequest($request);
-
-        if ($formulaire->isSubmitted() && $formulaire->isValid()) {
-            if ($livre->getNbStock() === null) {
-                $livre->setNbStock(1);
-            }
-
-            $gestionnaireEntite->persist($livre);
-            $gestionnaireEntite->flush();
-        
-            $origine = $request->query->get('origine');
-            if ($origine === 'mouvement_entree') {
-                return $this->redirectToRoute('app_mouvement_confirmation', [
-                    'id' => $livre->getId(),
-                    'type' => 'false' 
-                ]);
-            }
-
-            return $this->redirectToRoute('app_livre_index');
-        }
-
-        return $this->render('livre/new.html.twig', [
-            'livre' => $livre,
-            'form' => $formulaire,
-            // 'image_url' => $imagePreRemplie,
-        ]);
-    }
-
-    #[Route('/{id}/edit', name: 'app_livre_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Livre $livre, EntityManagerInterface $entityManager): Response
-    {
-        if (!$this->session->get('admin_authenticated')) {
-            return $this->redirectToRoute('app_admin_login');
-        }
-
-        $form = $this->createForm(LivreType::class, $livre);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
-
-            $this->addFlash('success', 'Le livre a été mis à jour avec succès.');
-            return $this->redirectToRoute('app_livre_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('livre/edit.html.twig', [
-            'livre' => $livre,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}/delete', name: 'app_livre_delete')]
-    public function delete(Request $request, Livre $livre, EntityManagerInterface $entityManager): Response
-    {
-        if (!$this->session->get('admin_authenticated')) {
-            return $this->redirectToRoute('app_admin_login');
-        }
-
-        if ($this->isCsrfTokenValid('delete'.$livre->getId(), $request->getPayload()->getString('_token'))) {
-            $entityManager->remove($livre);
-            $entityManager->flush();
-            $this->addFlash('success', 'Le livre a été supprimé.');
-        }
-
-        return $this->redirectToRoute('app_livre_index', [], Response::HTTP_SEE_OTHER);
-    }
-
-    #[Route('/recherche', name: 'app_livre_search', methods: ['GET'])]
-    public function search(Request $request, LivreRepository $livreRepository): Response
-    {
-        $isbn = $request->query->get('isbn'); 
-        $auteur = $request->query->get('auteur'); 
-
-        $qb = $livreRepository->createQueryBuilder('l');
-
-        if (!empty($isbn)) {
-            $qb->andWhere('l.isbn LIKE :isbnVal')
-               ->setParameter('isbnVal', '%' . $isbn . '%');
-        }
-        if (!empty($auteur)) {
-            $qb->andWhere('l.auteur LIKE :auteurVal')
-               ->setParameter('auteurVal', '%' . $auteur . '%');
-        }
-
-        $livres = $qb->orderBy('l.isbn', 'ASC')
-                     ->getQuery()
-                     ->getResult();
-
-        return $this->render('livre/index.html.twig', [
-            'livres' => $livres,
-            'last_isbn' => $isbn,
-            'last_auteur' => $auteur
-        ]);
-    }
-
-    #[Route('/check-google/{isbn}', name: 'app_livre_google_check')]
-    public function checkGoogleBooks(string $isbn, HttpClientInterface $httpClient, LivreRepository $livreRepository): Response
-    {
+        // 1. Recherche dans notre base de données locale
         $livreExistant = $livreRepository->findOneBy(['isbn' => $isbn]);
 
         if ($livreExistant) {
-            $this->addFlash('info', 'Livre déjà présent dans la bibliothèque.');
-
-            // On redirige vers la confirmation du mouvement (votre flux habituel)
-            return $this->redirectToRoute('app_mouvement_confirmation', [
-                'id' => $livreExistant->getId(),
-                'type' => 'false' // 'false' car on arrive souvent ici pour une Entrée
+            return $this->json([
+                'statut' => 'existe',
+                'id'     => $livreExistant->getId()
             ]);
         }
 
+        // 2. Recherche sur l'API Google Books si absent de la BDD
         try {
-            $response = $httpClient->request(
-                'GET',
-                'https://www.googleapis.com/books/v1/volumes?q=isbn:' . $isbn
-            );
-            $data = $response->toArray();
+            $reponse = $clientHttp->request('GET', 'https://www.googleapis.com/books/v1/volumes?q=isbn:' . $isbn);
+            $donnees = $reponse->toArray();
         } catch (\Exception $e) {
-            $data = [];
+            $donnees = [];
         }
 
-        if (!isset($data['items']) || $data['totalItems'] === 0) {
-            $this->addFlash('warning', 'Livre inconnu sur Google Books. Création manuelle.');
-            return $this->redirectToRoute('app_livre_new', [
-                'isbn_pre_rempli' => $isbn
+        // Si Google trouve le livre
+        if (isset($donnees['items']) && $donnees['totalItems'] > 0) {
+            $info = $donnees['items'][0]['volumeInfo'];
+            return $this->json([
+                'statut' => 'google',
+                'donnees' => [
+                    'isbn'        => $isbn,
+                    'titre'       => $info['title'] ?? '',
+                    'auteur'      => isset($info['authors']) ? implode(', ', $info['authors']) : '',
+                    'description' => $info['description'] ?? '',
+                    'image'       => $info['imageLinks']['thumbnail'] ?? ''
+                ]
             ]);
         }
 
-        $info = $data['items'][0]['volumeInfo'];
-        $this->addFlash('success', 'Livre trouvé sur Google !');
-
-        return $this->redirectToRoute('app_livre_new', [
-            'isbn_pre_rempli'         => $isbn,
-            'titre_pre_rempli'        => $info['title'] ?? '',
-            'auteur_pre_rempli'       => isset($info['authors']) ? implode(', ', $info['authors']) : '',
-            'description_pre_remplie' => $info['description'] ?? '',
-            'image_pre_remplie'       => $info['imageLinks']['thumbnail'] ?? '',
-            'origine'                 => 'mouvement_entree' 
+        // 3. Cas où le livre n'est nulle part
+        return $this->json([
+            'statut' => 'inconnu', 
+            'isbn' => $isbn
         ]);
+    }
+
+    /**
+     * Retourne le formulaire de création de livre (fragment HTML pour modale).
+     * Interagit avec : _modal_new.html.twig et le script JS principal.
+     */
+    #[Route('/nouveau', name: 'app_livre_new', methods: ['GET', 'POST'])]
+    public function new(Request $requete, EntityManagerInterface $em): Response
+    {
+        $livre = new Livre();
+
+        // On pré-remplit l'objet Livre avec les données reçues en GET (provenant de l'API Google)
+        $livre->setIsbn($requete->query->get('isbn'));
+        $livre->setNom($requete->query->get('titre'));
+        $livre->setAuteur($requete->query->get('auteur'));
+        $livre->setDescription($requete->query->get('description'));
+        $livre->setLienImg($requete->query->get('image'));
+        $livre->setNbStock(0); // Initialisation du stock
+
+        $formulaire = $this->createForm(LivreType::class, $livre);
+        $formulaire->handleRequest($requete);
+
+        if ($formulaire->isSubmitted() && $formulaire->isValid()) {
+            $em->persist($livre);
+            $em->flush();
+
+            // Réponse JSON pour indiquer au JS que la création est réussie
+            return $this->json([
+                'success' => true,
+                'id'      => $livre->getId()
+            ]);
+        }
+
+        // Retourne uniquement le fragment Twig pour l'affichage en modale
+        return $this->render('livre/_modal_new.html.twig', [
+            'form' => $formulaire->createView(),
+        ], new Response(null, $formulaire->isSubmitted() ? 422 : 200));
     }
 }
--- src/Controller/MouvementController.php
+++ src/Controller/MouvementController.php
@@ -1,17 +1,9 @@
 <?php
 
-/* @author : Dufour Marc (marc.dufour@stjosup.com)
- * @version : 1
- * @dateCreate : 12/01/2026
- * @lastUpdate : 19/01/2026
- */
-
 namespace App\Controller;
 
-use App\Entity\Livre;
 use App\Entity\Mouvement;
 use App\Repository\LivreRepository;
-use App\Repository\MouvementRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -21,170 +13,65 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/mouvement')]
 class MouvementController extends AbstractController
 {
-    #[Route('/', name:'app_mouvement_index')]
-    public function index(MouvementRepository $mouvements) : Response
+    /**
+     * Retourne le formulaire de scan ISBN (fragment HTML pour modale).
+     * Interagit avec : _modal_debut.html.twig
+     */
+    #[Route('/debut-fragment', name: 'app_mouvement_debut_fragment')]
+    public function debutFragment(Request $requete): Response
     {
-        return $this->render('mouvement/index.html.twig', [
-            'mouvements' => $mouvements->findBy([], ['dateHeure' => 'DESC']),
-        ]);
-    }
-    #[Route('/debut/{action}', name: 'app_mouvement_debut', defaults: ['action' => null])]
-    public function debut(?string $action, Request $request): Response
-    {
-        // Si c'est une requête AJAX, on ne renvoie que le fragment Twig
-        if ($request->isXmlHttpRequest()) {
-            return $this->render('mouvement/_modal_debut.html.twig', [
-                'selection_defaut' => $action
-            ]);
-        }
+        // 'action' contient "true" (Sortie) ou "false" (Entrée)
+        $actionMouvement = $requete->query->get('action'); 
 
-        // Sinon, on renvoie la page complète habituelle 
-        return $this->render('mouvement/debut.html.twig', [
-            'selection_defaut' => $action
+        return $this->render('mouvement/_modal_debut.html.twig', [
+            'selection_defaut' => $actionMouvement
         ]);
     }
 
-    #[Route('/show/{id<\d+>}', name: 'app_mouvement_show')]
-    public function show(Mouvement $mouvement, Request $request): Response
+    /**
+     * Retourne le formulaire de confirmation (fragment HTML pour modale).
+     * Interagit avec : _modal_confirmation.html.twig
+     */
+    #[Route('/confirmation-fragment/{id<\d+>}', name: 'app_mouvement_confirmation_fragment')]
+    public function confirmationFragment(int $id, Request $requete, LivreRepository $livreRepo): Response
     {
-        // Si c'est une requête AJAX, on ne renvoie que le fragment Twig
-        if ($request->isXmlHttpRequest()) {
-            return $this->render('mouvement/_mouvement_details.html.twig', [
-                'mouvement' => $mouvement,
-            ]);
-        }
+        $livre = $livreRepo->find($id);
+        $estUneSortie = $requete->query->get('type_action') === 'true';
 
-        // Sinon, on renvoie la page complète habituelle 
-        return $this->render('mouvement/show.html.twig', [
-            'mouvement' => $mouvement,
+        return $this->render('mouvement/_modal_confirmation.html.twig', [
+            'livre'     => $livre,
+            'estSortie' => $estUneSortie
         ]);
     }
 
-    #[Route('/verification', name: 'app_mouvement_verification', methods: ['POST'])]
-    public function verificationIsbn(Request $requete, LivreRepository $depotLivre): Response
+    /**
+     * Enregistre le mouvement final et met à jour le stock du livre.
+     * Interagit avec : Le formulaire de confirmation en AJAX.
+     */
+    #[Route('/finaliser/{id<\d+>}', name: 'app_mouvement_finaliser', methods: ['POST'])]
+    public function finaliser(int $id, Request $requete, LivreRepository $livreRepo, EntityManagerInterface $em): Response
     {
-        $isbnRecu = $requete->request->get('isbn');
-        $typeAction = $requete->request->get('type_action'); 
+        $livre          = $livreRepo->find($id);
+        $typeSortie     = $requete->request->get('type_action') === 'true';
+        $nomUtilisateur = $requete->request->get('nomPrenom');
 
-        $livre = $depotLivre->findOneBy(['isbn' => $isbnRecu]);
-
-        /// Cas SORTIE (true)
-        if ($typeAction === 'true') {
-            if (!$livre) {
-                $this->addFlash('error', 'Livre inconnu. Impossible de sortir un livre absent de l\'inventaire.');
-                return $this->redirectToRoute('app_mouvement_debut', ['action' => 'true']);
-            }
-        
-            return $this->redirectToRoute('app_mouvement_confirmation', [
-                'id' => $livre->getId(),
-                'type' => 'true' 
-            ]);
-        } 
-        // Cas ENTRÉE (false)
-        else {
-            if ($livre) {
-                return $this->redirectToRoute('app_mouvement_confirmation', [
-                    'id' => $livre->getId(),
-                    'type' => 'false'
-                ]);
-            } 
-            else {
-                return $this->redirectToRoute('app_livre_google_check', [
-                    'isbn' => $isbnRecu,
-                    'origine' => 'mouvement_entree'
-                ]);
-            }
-        }
-    }
-
-    #[Route('/confirmation/{id}/{type}', name: 'app_mouvement_confirmation', methods: ['GET'])]
-    public function confirmation(Livre $livre, string $type): Response 
-    {
-        return $this->render('mouvement/confirmation.html.twig', [
-            'livre' => $livre,
-            'estSortie' => ($type === 'true') 
-        ]);
-    }
-
-    #[Route('/finaliser/{id}', name: 'app_mouvement_finaliser', methods: ['POST'])]
-    public function finaliserMouvement(
-        Livre $livre, 
-        Request $requete, 
-        EntityManagerInterface $gestionnaireEntite
-    ): Response {
-        $nomPrenom = $requete->request->get('nomPrenom');
-        
-        $typeRaw = $requete->request->get('type_action'); 
-        $estUneSortie = ($typeRaw === 'true' || $typeRaw === 1);
-
+        // Création de l'entité Mouvement
         $mouvement = new Mouvement();
-        $mouvement->setLivre($livre); 
-        $mouvement->setNomPrenom($nomPrenom);
-        $mouvement->setType($estUneSortie); 
-        $mouvement->setDateHeure(new \DateTime());
+        $mouvement->setLivre($livre);
+        $mouvement->setNomPrenom($nomUtilisateur);
+        $mouvement->setDateHeure(new \DateTimeImmutable());
+        $mouvement->setType($typeSortie);
 
-        $stockActuel = $livre->getNbStock();
-
-        if ($estUneSortie) { 
-            // CAS SORTIE (Retrait)
-            if ($stockActuel > 0) {
-                $livre->setNbStock($stockActuel - 1);
-            } else {
-                $livre->setNbStock(0);
-                $this->addFlash('warning', 'Le stock était déjà à 0.');
-            }
+        // Mise à jour logique du stock
+        if ($typeSortie) {
+            $livre->setNbStock($livre->getNbStock() - 1);
         } else {
-            // CAS ENTRÉE (Depot)
-            $livre->setNbStock($stockActuel + 1);
+            $livre->setNbStock($livre->getNbStock() + 1);
         }
 
-        $gestionnaireEntite->persist($mouvement);
-        $gestionnaireEntite->persist($livre);
-        $gestionnaireEntite->flush();
+        $em->persist($mouvement);
+        $em->flush();
 
-        $this->addFlash('success', 'Mouvement enregistré avec succès !');
-
-        return $this->redirectToRoute('home_index');
-    }
-
-    #[Route('/recherche', name: 'app_mouvement_search', methods: ['GET'])]
-    public function search(Request $request, MouvementRepository $mouvementRepository): Response
-    {
-        $isbn = $request->query->get('isbn');
-        $auteur = $request->query->get('auteur');
-        $user = $request->query->get('user');
-        $direction = $request->query->get('sort', 'DESC');
-
-        $qb = $mouvementRepository->createQueryBuilder('m')
-            ->leftJoin('m.livre', 'l')
-            ->addSelect('l');
-
-        if (!empty($isbn)) {
-            $qb->andWhere('l.isbn LIKE :isbn')
-               ->setParameter('isbn', '%' . $isbn . '%');
-        }
-
-        if (!empty($auteur)) {
-            $qb->andWhere('l.auteur LIKE :auteur')
-               ->setParameter('auteur', '%' . $auteur . '%');
-        }
-
-        // Recherche sur le nom de la personne ayant fait le mouvement
-        if (!empty($user)) {
-            $qb->andWhere('m.nomPrenom LIKE :user')
-               ->setParameter('user', '%' . $user . '%');
-        }
-
-        $mouvements = $qb->orderBy('m.dateHeure', $direction)
-                         ->getQuery()
-                         ->getResult();
-
-        return $this->render('mouvement/index.html.twig', [
-            'mouvements' => $mouvements,
-            'last_isbn' => $isbn,
-            'last_auteur' => $auteur,
-            'last_user' => $user, 
-            'current_sort' => $direction,
-        ]);
+        return $this->json(['success' => true]);
     }
 }
