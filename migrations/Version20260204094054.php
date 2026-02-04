f17aeeb419da4cdbd463804625bd4b8ae00a3795
unknown <stagiaire@W2003IES.LOCAL>
2026-02-04 10:41:31 +0100

    + MàJ bdd pour genre

---
+++ migrations/Version20260204094054.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260204094054 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE livre ADD COLUMN genre VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TEMPORARY TABLE __temp__livre AS SELECT id, isbn, titre, auteur, description, lien_img, nb_stock, actif FROM livre');
+        $this->addSql('DROP TABLE livre');
+        $this->addSql('CREATE TABLE livre (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, isbn VARCHAR(255) NOT NULL, titre VARCHAR(255) NOT NULL, auteur VARCHAR(255) NOT NULL, description VARCHAR(255) DEFAULT NULL, lien_img VARCHAR(255) DEFAULT NULL, nb_stock INTEGER NOT NULL, actif BOOLEAN DEFAULT NULL)');
+        $this->addSql('INSERT INTO livre (id, isbn, titre, auteur, description, lien_img, nb_stock, actif) SELECT id, isbn, titre, auteur, description, lien_img, nb_stock, actif FROM __temp__livre');
+        $this->addSql('DROP TABLE __temp__livre');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_AC634F99CC1CF4E6 ON livre (isbn)');
+    }
+}
